655912bee2a7d8773a34c066c9c20d2d832004ce
lssouzadev <leoss91@hotmail.com>
2022-12-15 14:28:07 -0300

    aplicando responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,39 @@ import styles from './App.module.css';
 import './global.css'
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lssouzadev.png',
+      name: 'Leonardo Souza',
+      role: 'Student'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+                  
+    ],
+    publishedAt: new Date('2022-12-15 12:14:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lssouzadev.png',
+      name: 'Leticia Lima',
+      role: 'Cirurgiã Dentista'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+                  
+    ],
+    publishedAt: new Date('2022-12-10 12:14:00')
+  },
+];
+
 export function App() {
   return (
     <div>
